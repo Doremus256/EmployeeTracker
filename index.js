58877ba23112fddf7ba3d6781699d0364341e2df
Brad Doremus <rbdoremus13@gmail.com>
2020-09-07 18:30:59 -0500

    Added switch/cases for command line user interface

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
 const cTable = require("console.table");
 
 var connection = mysql.createConnection( {
@@ -13,5 +13,49 @@ var connection = mysql.createConnection( {
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    init();
 });
+
+function init() {
+    inquirer.prompt([{
+        message: "Dynamic Employee DB: What would you like to do?",
+        name: "searchType",
+        type: "list",
+        choices: [
+            "View All Employees",
+            "View All Employees by Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Position",
+            "Update Employee Manager",
+            "View All Positions",
+        ]
+    }])
+        .then(({ searchType }) => {
+            switch (searchType) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "View All Employees by Manager":
+                    viewEmployeesByManager();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+                case "Update Employee Position":
+                    updateEmployeePosition();
+                    break;
+                case "Update Employee Manager":
+                    updateEmployeeManager();
+                    break;
+                case "View All Positions":
+                    viewAllPositions();
+                    break;
+                default: connection.end();
+    }})
+}
+
 
