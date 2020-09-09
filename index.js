0ee794ff7ae42fd1db0033d74a455ade8fc0beaa
Brad Doremus <rbdoremus13@gmail.com>
2020-09-08 20:00:30 -0500

    Only 2 more options left to code

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ function init() {
         choices: [
             "View All Employees",
             "View All Employees by Manager",
+            "View Departments",
             "Add Employee",
+            "Add Position",
+            "Add Department",
             "Remove Employee",
             "Update Employee Position",
             "Update Employee Manager",
@@ -44,6 +47,9 @@ function init() {
                 case "View All Employees by Manager":
                     viewEmployeesByManager();
                     break;
+                case "View Departments":
+                    viewDepartments();
+                    break;
                 case "Add Employee":
                     addEmployee();
                     break;
@@ -53,8 +59,11 @@ function init() {
                 case "Update Employee Position":
                     updateEmployeePosition();
                     break;
-                case "Update Employee Manager":
-                    updateEmployeeManager();
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Position":
+                    addPosition();
                     break;
                 case "View All Positions":
                     viewAllPositions();
@@ -69,21 +78,76 @@ function init() {
 
 // Print to console as table (console.table): List of all current employees. SELECT *(all) FROM employee(table)
 function viewAllEmployees() {
-    connection.query("SELECT e.id, e.first_name AS First Name, e.last_name AS Last Name, e.position_id AS Position, e.manager_id AS manager, FROM employees, INNER JOIN employees ON positions.title = employees.id", function (err, res) {
+    connection.query("SELECT * FROM employees", function (err, res) {
         if (err) throw (err)
         console.table(res)
+        init();
     })
 };
+
+function viewAllPositions() {
+    connection.query("SELECT * FROM positions", function (err, res) {
+        if (err) throw (err)
+        console.table(res)
+        init();
+})
+};
+
+function viewDepartments() {
+    connection.query("SELECT * FROM departments", function (err, res) {
+        if (err) throw (err)
+        console.table(res)
+        init();
+})
+};
+
+function addDepartment() {
+    inquirer.prompt([{
+        message: "Enter the department name:",
+        name: "newDepartment",
+        type: "input",
+    }]) .then (function(response){
+        connection.query("INSERT INTO departments (deptname) VALUES (?)", response.newDepartment,  function (err, res) {
+            if (err) throw (err)
+            console.table(res)
+            init();
+    })}
+    )};
+
+    function addPosition() {
+        inquirer.prompt([
+            {
+            message: "Enter the position title:",
+            name: "newPosition",
+            type: "input",
+        }, {
+            message: "Enter the position salary:",
+            name: "newSalary",
+            type: "input",
+        },{
+            message: "Enter the position department id:",
+            name: "newDeptId",
+            type: "list",
+            choices: [
+                1, 2, 3, 4
+            ]
+        }]) .then (function(response){
+            connection.query("INSERT INTO positions (title, salary, dept_id) VALUES (?,?,?)", [response.newPosition, response.newSalary, response.newDeptId], function (err, res) {
+                if (err) throw (err)
+                console.table(res)
+                init();
+        })}
+        )};
 
 function viewEmployeesByManager() {
     inquirer.prompt([{
         message: "Enter manager name to see his/her staff:",
         name: "employeesByManager",
         type: "input",
-    }]) .then; {
-    connection.query("SELECT * FROM employees WHERE manager_id = res")
-    
-    // need to use INNER JOIN here but return value not id //
+    }]).then; {
+        connection.query("SELECT * FROM employees WHERE manager_id = res")
+
+        // need to use INNER JOIN here but return value not id //
     }
 }
 
